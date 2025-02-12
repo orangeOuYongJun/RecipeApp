e9de890b5d8e75d400b9026d09d4321fa9253796
YongjunOu <yongjun@micro-leads.com>
2025-02-12 16:49:52 -0500

    init project

---
+++ Recipes/Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Yongjun Ou on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
